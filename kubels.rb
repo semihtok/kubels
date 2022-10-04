6cd6abd533ee443e95eda08933881e006710acd4
semihtok <ibrahim@semihtok.com>
2022-10-04 11:27:21 +0300

    Brew formula update for kubels version v0.2.1

--- kubels.rb
+++ kubels.rb
@@ -5,15 +5,15 @@
 class Kubels < Formula
   desc "kubels (kube ls) is a Kubernetes tool that allows you to list Kubernetes resources with a simple command(s)."
   homepage "https://github.com/semihtok/kubels"
-  version "0.2.0"
+  version "0.2.1"
   license "Apache 2.0"
 
   depends_on "git"
   depends_on "go"
 
   on_macos do
-    url "https://github.com/semihtok/kubels/releases/download/v0.2.0/kubels_0.2.0_darwin_amd64.tar.gz"
-    sha256 "858cebc3863fde9333b341df41fc2705584ba73fe1179ff41a5b157aace04d0b"
+    url "https://github.com/semihtok/kubels/releases/download/v0.2.1/kubels_0.2.1_darwin_amd64.tar.gz"
+    sha256 "30f797846626f7419a5fe82eea304190c603008630b546ac9fe3c1709294e4ac"
 
     def install
       bin.install "kubels"
@@ -32,16 +32,16 @@ class Kubels < Formula
 
   on_linux do
     if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/semihtok/kubels/releases/download/v0.2.0/kubels_0.2.0_linux_armv6.tar.gz"
-      sha256 "9a8037f8151e94bf78910e6454d2be4cee209fa03cac2f9624c4529b30cffb91"
+      url "https://github.com/semihtok/kubels/releases/download/v0.2.1/kubels_0.2.1_linux_armv6.tar.gz"
+      sha256 "839c0451605c90ad0a221384cd7cb797990a35305620a375fcb42628ce51fdb4"
 
       def install
         bin.install "kubels"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/semihtok/kubels/releases/download/v0.2.0/kubels_0.2.0_linux_amd64.tar.gz"
-      sha256 "c0b6c13450c2b166845668ffd6d8352e8b1c9e58c01ca6daf69b438d7fb376ff"
+      url "https://github.com/semihtok/kubels/releases/download/v0.2.1/kubels_0.2.1_linux_amd64.tar.gz"
+      sha256 "2fba15bace295e3a62070921762a8f05bc417695864e47cedd82a744bf10894b"
 
       def install
         bin.install "kubels"
